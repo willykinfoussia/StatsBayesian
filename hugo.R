2bc8e762c5c35ccedf27295895d94249af80e557
hugop <hugo.picard@live.fr>
2024-03-30 14:57:57 +0100

    fake_data

--- hugo.R
+++ hugo.R
@@ -53,6 +53,44 @@ sentiment_columns <- function(data, columns){
 
 
 
+generate_fake_data <- function(data){
+  
+  columns_to_exclude <- c("percenttp", "wastecents", "feelfedgov_1", "weightvec")
+  columns_to_convert <- setdiff(names(Data), columns_to_exclude)
+  
+  n = dim(Data)[1]
+  p_gender = table(Data$gender)/n
+  gender_fake = sample(1:4, size=n, replace=TRUE, prob=p_gender)
+  fake_data = data.frame(gender = as.factor(gender_fake))
+  
+  for (i in columns_to_convert){
+    p = tabulate(as.integer(sapply(Data[i], function(x) x + ifelse(min(Data[i]) == 0, 1, 0))))/n
+    d_fake = sample(min(Data[i]):max(Data[i]), size=n, replace=TRUE, prob=p)
+    fake_data[i] = d_fake
+  }
+  fake_data$wastecents = pmin(100, pmax(0, as.integer(rnorm(n, mean = mean(Data$wastecents), sd = sd(Data$wastecents)))))
+  fake_data$feelfedgov_1 = pmin(100, pmax(0, as.integer(rnorm(n, mean = mean(Data$feelfedgov_1), sd = sd(Data$feelfedgov_1)))))
+  fake_data$weightvec = pmax(0, rnorm(n, mean = mean(Data$weightvec), sd = sd(Data$weightvec)))
+  
+  
+  coef = c()
+  for (i in colnames(fake_data)){
+    res <- lm(percenttp ~ get(i), data=Data)
+    coef <- c(coef, res$coefficients[2])
+  }
+  coef = c(coef, 50)
+  fake_data$percenttp = rep(1, n)
+  
+  
+  for (i in 1:dim(fake_data)[1]){
+    fake_data$percenttp[i] = as.integer(sum(coef * fake_data[i,]))
+  }
+  
+  names(coef) = colnames(fake_data)
+  
+  return(list(fake_data = fake_data, coef = coef))
+}
+
 
 
 
